bd9a71314ec793845cefb0437ab8f7bd51fa5bb5
Karl Preisner <karl@preisner.us>
2023-12-19 18:27:42 -0600

    add missing line to build

--- build-docs.sh
+++ build-docs.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 mkdir -p docs/docstrings
 
+set -e
+
 # pip install lazydocs
 lazydocs \
     --output-path="./docs/docstrings" \
     --overview-file="README.md" \
     --src-base-url="https://github.com/Sindri-Labs/sindri-python/blob/main/" \
     src/sindri_labs
+
+mkdocs build
 
 # Transform markdown to jsx syntax so it can be rendered in Docusaurus
 python3 _transform_md_to_jsx.py
