11a3e26585e72c0a6c2d80fb93f4c8843552775e
Karl Preisner <karl@preisner.us>
2023-12-19 18:18:57 -0600

    change build so it generated jsx docs that will be copied from a separate repos deployment action

--- local-run-docs.sh
+++ local-run-docs.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 mkdir -p docs/docstrings
 
+set -e
 
+# pip install lazydocs
 lazydocs \
     --output-path="./docs/docstrings" \
     --overview-file="README.md" \
     --src-base-url="https://github.com/Sindri-Labs/sindri-python/blob/main/" \
+    --no-watermark \
     src/sindri_labs
 
+# Transform markdown to jsx syntax so it can be rendered in Docusaurus
+python3 _transform_md_to_jsx.py
+
+# pip install mkdocs mkdocs-awesome-pages-plugin
 echo "Hosting docs on http://localhost:1111"
 mkdocs serve -a localhost:1111
