9cf006b80792e7b68cbd633cafcc3d0d8fc88333
Karl Preisner <karl@preisner.us>
2023-12-19 18:54:23 -0600

    remove unused requirements

--- local-run-docs.sh
+++ local-run-docs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p docs/docstrings
 
-# pip install lazydocs
+# pip install requests>=2.13.0 lazydocs
 lazydocs \
     --output-path="./docs/docstrings" \
     --overview-file="README.md" \
