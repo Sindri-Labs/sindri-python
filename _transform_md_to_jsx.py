c8f16459c2848c41786621fe83173c9011a3bea5
Karl Preisner <karl@preisner.us>
2023-12-20 08:35:44 -0600

    remove unused doc generation script

--- _transform_md_to_jsx.py
+++
@@ -1,7 +0,0 @@
-with open("docs/docstrings/sindri.md", "r") as read_file, open("docs/docstrings/sindri.jsx.md", "w") as write_file:
-    for line in read_file.readlines():
-        line_new = line
-        if line.startswith("<a href=") and '<img align="right" style="float:right;" src="https://img.shields.io' in line:
-            line_new = line.replace("></a>", "/></a>")
-            line_new = line_new.replace('style="float:right;"', 'style={{float:"right"}}')
-        write_file.write(line_new)
--- build-docs.sh
+++ build-docs.sh
@@ -8,6 +8,3 @@ lazydocs \
     --src-base-url="https://github.com/Sindri-Labs/sindri-python/blob/main/" \
     --no-watermark \
     src/sindri_labs
-
-# Transform markdown to jsx syntax so it can be rendered in Docusaurus
-python3 _transform_md_to_jsx.py
