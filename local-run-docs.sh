498a129f46747794146e0c55b25818b4d1b40cf3
Karl Preisner <karl@preisner.us>
2023-12-27 11:11:27 -0500

    supply github_ref as $TAG for lazydocs --src-base-url

--- local-run-docs.sh
+++ local-run-docs.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 mkdir -p docs/docstrings
 
+
+TAG="${TAG:-main}"
+
 # pip install requests>=2.13.0 lazydocs
 lazydocs \
     --output-path="./docs/docstrings" \
     --overview-file="README.md" \
-    --src-base-url="https://github.com/Sindri-Labs/sindri-python/blob/main/" \
+    --src-base-url="https://github.com/Sindri-Labs/sindri-python/blob/$TAG/" \
     --no-watermark \
     src/sindri_labs
 
