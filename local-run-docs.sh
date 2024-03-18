a790c9cacc7d8f90ba0018afd33929a170802f9e
Karl Preisner <karl@sindri.app>
2024-03-18 14:13:42 +0000

    docs build was not working with the introduction of __init__.py

--- local-run-docs.sh
+++ local-run-docs.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
+
+set -e
+
 mkdir -p docs/docstrings
 
 
 TAG="${TAG:-main}"
 
-# pip install requests>=2.13.0 lazydocs
+# pip install requests>=2.13.0 lazydocs==0.4.8
 lazydocs \
     --output-path="./docs/docstrings" \
     --overview-file="README.md" \
     --src-base-url="https://github.com/Sindri-Labs/sindri-python/blob/$TAG/" \
     --no-watermark \
-    src/sindri
+    src/sindri/sindri.py
 
 # pip install mkdocs mkdocs-awesome-pages-plugin
 echo "Hosting docs on http://localhost:1111"
